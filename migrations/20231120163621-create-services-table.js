3118dd593511eb42edb51a159aecdb671b7d9cd9
adinrama <adinrama@student.ub.ac.id>
2023-11-20 23:52:00 +0700

    services migration

---
+++ migrations/20231120163621-create-services-table.js
@@ -0,0 +1,57 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("services", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      hospitalId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: "hospitals",
+          },
+          key: "id",
+        },
+      },
+      vaccin: {
+        type: Sequelize.ENUM,
+        values: ["yes", "no"],
+        defaultValue: "no",
+      },
+      pcrTest: {
+        type: Sequelize.ENUM,
+        values: ["yes", "no"],
+        defaultValue: "no",
+      },
+      rapidAntigenTest: {
+        type: Sequelize.ENUM,
+        values: ["yes", "no"],
+        defaultValue: "no",
+      },
+      genose: {
+        type: Sequelize.ENUM,
+        values: ["yes", "no"],
+        defaultValue: "no",
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("services");
+  },
+};
---
+++ models/Service.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      hospitalId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      vaccin: {
+        type: DataTypes.ENUM,
+        values: ["yes", "no"],
+        defaultValue: "no",
+      },
+      pcrTest: {
+        type: DataTypes.ENUM,
+        values: ["yes", "no"],
+        defaultValue: "no",
+      },
+      rapidAntigenTest: {
+        type: DataTypes.ENUM,
+        values: ["yes", "no"],
+        defaultValue: "no",
+      },
+      genose: {
+        type: DataTypes.ENUM,
+        values: ["yes", "no"],
+        defaultValue: "no",
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    { tableName: "services" }
+  );
+};
