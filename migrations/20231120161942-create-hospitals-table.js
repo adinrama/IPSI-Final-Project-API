84e88765ba2290831f7495b5499ad11a06adfa8e
adinrama <adinrama@student.ub.ac.id>
2023-11-20 23:35:01 +0700

    hospitals migration

---
+++ migrations/20231120161942-create-hospitals-table.js
@@ -0,0 +1,67 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("hospitals", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      rsAdminId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: "users",
+          },
+          key: "id",
+        },
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      numberCode: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      telp: {
+        type: Sequelize.STRING(13),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      class: {
+        type: Sequelize.ENUM,
+        values: ["A", "B", "C", "D"],
+        allowNull: false,
+      },
+      usage_status: Sequelize.STRING,
+      owner: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("hospitals");
+  },
+};
---
+++ models/Hospital.js
@@ -0,0 +1,56 @@
+module.exports = (sequelize, DataTypes) => {
+  const Hospital = sequelize.define(
+    "Hospital",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      rsAdminId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      numberCode: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      telp: {
+        type: DataTypes.STRING(13),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      class: {
+        type: DataTypes.ENUM,
+        values: ["A", "B", "C", "D"],
+        allowNull: false,
+      },
+      usage_status: DataTypes.STRING,
+      owner: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    { tableName: "hospitals" }
+  );
+};
