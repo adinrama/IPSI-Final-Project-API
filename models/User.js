90064c7dc31842848ec6c36e09088e6635f04545
adinrama <adinrama@student.ub.ac.id>
2023-11-23 03:06:21 +0700

    add bcrypt on user password

--- models/User.js
+++ models/User.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     "User",
@@ -56,6 +58,17 @@ module.exports = (sequelize, DataTypes) => {
     },
     { tableName: "users" }
   );
+
+  User.beforeCreate(async (user) => {
+    if (user.password) {
+      const saltRounds = 10;
+      user.password = await bcrypt.hash(user.password, saltRounds);
+    }
+  });
+
+  User.prototype.validPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
 
   return User;
 };
